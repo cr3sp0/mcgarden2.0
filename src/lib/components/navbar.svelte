b4a635c1fc593b43b7681f0da9cd629a78b63066
Alessio Crespolini <crespolini.alessio@gmail.com>
2026-07-07 12:17:31 +0200

    Project init and navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,53 @@
+<div class="navbar">
+    <div class="nav-left nav-section"></div>
+    <div class="logo"></div>
+    <div class="nav-right nav-section">
+        <a href="*">Chi siamo</a>
+        <a href="*">Servizi</a>
+        <a href="*">CONTATTACI</a>
+    </div>
+</div>
+
+<style>
+
+    .navbar {
+        width: 100%;
+        height: var(--nav-h, 70px);
+        
+        display: flex;
+        flex-direction: row;
+
+        position: sticky;
+        top: 0;
+        z-index: 100;
+    }
+
+    .nav-section {
+        flex: 1; 
+    }
+
+    .nav-left {
+        background-color: blue;
+    }
+
+    .logo {
+        width: 100px;
+        background-color: blueviolet;
+        flex-shrink: 0; 
+    }
+
+    .nav-right {
+        display: flex; 
+        flex-direction: row;
+        align-items: center;
+        justify-content: flex-end; 
+        gap: 5rem;
+        background-color: red;
+        padding-right: 2rem;
+    }
+    
+    a {
+        color: white;
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Navbar from "$lib/components/navbar.svelte";
+
+</script>
+<div class="container">
+    <div class="hero">
+        <Navbar />
+    </div>
+</div>
+
+<style>
+    .container {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+    }
+
+    .hero {
+        background-color: #429C48;
+        width: 100%;
+        height: 100vh;
+        padding: 0 50px;
+        box-sizing: border-box;
+    }
+</style>
